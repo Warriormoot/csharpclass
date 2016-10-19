c47e500ed9ed7d36a20c538954d425f15cb80c38
Matthew Sargent <mattsargent23penguins@yahoo.com>
2016-10-19 11:53:45 -0600

    Commited Csharp to GIT

--- PowerUps.cs
+++ PowerUps.cs
@@ -2,11 +2,16 @@ using System;
 
 public class PowerUp {
 
-    public int power = 10;
+    public power = 10;
 
     public float duration = 2;
 
     public void RunPowerUp () {
         console.WriteLine("Powerup Ran");
+    }
+
+    public void RechargePowerUp (int _power) {
+        power += _power;
+        Console.WriteLine("Recharged PowerUp to" + power);
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,15 +3,17 @@ using System;
 public class Program
 {
 
-    public static PowerUp Health;
-
-    PowerUp Ammo;
-
+    public static PowerUp Health = new PowerUp();
+    public static PowerUp Ammo = new PowerUp();
     public static void Main(String[] args)
     {
         Health.duration = 10;
         Health.RunPowerUp(); //object.functionName To call an object 
         Console.WriteLine(Health.duration);
+        Ammo.RunPowerUp();
         //Console.WriteLine(myString);
+
+        Health.RechargePowerUp(20);
+        Ammo.RechargePowerUp(300);
     }
 }
